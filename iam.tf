2cd7fc9670ac262fdcbd93b4c7e4d3b0a9b08619
Ciprian <cursu@axetrading.com>
2023-03-24 10:27:07 +0200

    adding lambda vpc access policy

--- iam.tf
+++ iam.tf
@@ -38,6 +38,13 @@ resource "aws_iam_role_policy_attachment" "lambda" {
   policy_arn = each.value
 }
 
+resource "aws_iam_role_policy_attachment" "vpc_execution_role" {
+  count      = var.subnet_ids != null && (var.security_group_ids != null || var.create_security_group) ? 1 : 0
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+  role       = aws_iam_role.lambda[0].name
+}
+
+
 module "short-name" {
   count      = local.lambda_role_prefix != null ? 1 : 0
   source     = "axetrading/short-name/null"
