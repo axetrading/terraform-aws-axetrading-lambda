06149c0e7115d44411362a4ee6017834f4c0645a
Ciprian <cursu@axetrading.com>
2023-06-02 16:27:27 +0300

    removing jsonencode

--- modules/cloudwatch-log-group-trigger/main.tf
+++ modules/cloudwatch-log-group-trigger/main.tf
@@ -1,12 +1,10 @@
 resource "aws_cloudwatch_event_rule" "this" {
   name           = var.event_rule_name
   event_bus_name = var.event_bus_name
-  event_pattern = jsonencode(templatefile(
+  event_pattern = templatefile(
     "${path.module}/event_pattern.tpl", {
       log_group_name_prefixes = var.log_group_name_prefixes
-    }
-    )
-  )
+  })
   is_enabled = true
 }
 
